68ac850ba42fed11c6b20459b9de623fb2e1c5e2
dhaas01 <77082867+dhaas01@users.noreply.github.com>
2021-05-12 22:01:15 -0400

    Add files via upload

---
+++ Distance Calculator (Number 1)/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Distance_Calculator__Number_1_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.mphTextBox = new System.Windows.Forms.TextBox();
+            this.hoursTextBox = new System.Windows.Forms.TextBox();
+            this.mphLabel = new System.Windows.Forms.Label();
+            this.hoursLabel = new System.Windows.Forms.Label();
+            this.distanceListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(89, 275);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(75, 23);
+            this.calculateButton.TabIndex = 0;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.CalculateButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(213, 275);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 1;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // mphTextBox
+            // 
+            this.mphTextBox.Location = new System.Drawing.Point(213, 21);
+            this.mphTextBox.Name = "mphTextBox";
+            this.mphTextBox.Size = new System.Drawing.Size(100, 20);
+            this.mphTextBox.TabIndex = 2;
+            // 
+            // hoursTextBox
+            // 
+            this.hoursTextBox.Location = new System.Drawing.Point(213, 58);
+            this.hoursTextBox.Name = "hoursTextBox";
+            this.hoursTextBox.Size = new System.Drawing.Size(100, 20);
+            this.hoursTextBox.TabIndex = 3;
+            // 
+            // mphLabel
+            // 
+            this.mphLabel.AutoSize = true;
+            this.mphLabel.Location = new System.Drawing.Point(54, 24);
+            this.mphLabel.Name = "mphLabel";
+            this.mphLabel.Size = new System.Drawing.Size(117, 13);
+            this.mphLabel.TabIndex = 4;
+            this.mphLabel.Text = "Vehicle Speed in MPH:";
+            // 
+            // hoursLabel
+            // 
+            this.hoursLabel.AutoSize = true;
+            this.hoursLabel.Location = new System.Drawing.Point(88, 61);
+            this.hoursLabel.Name = "hoursLabel";
+            this.hoursLabel.Size = new System.Drawing.Size(83, 13);
+            this.hoursLabel.TabIndex = 5;
+            this.hoursLabel.Text = "Hours Traveled:";
+            // 
+            // distanceListBox
+            // 
+            this.distanceListBox.FormattingEnabled = true;
+            this.distanceListBox.Location = new System.Drawing.Point(57, 96);
+            this.distanceListBox.Name = "distanceListBox";
+            this.distanceListBox.Size = new System.Drawing.Size(256, 160);
+            this.distanceListBox.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(376, 310);
+            this.Controls.Add(this.distanceListBox);
+            this.Controls.Add(this.hoursLabel);
+            this.Controls.Add(this.mphLabel);
+            this.Controls.Add(this.hoursTextBox);
+            this.Controls.Add(this.mphTextBox);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.calculateButton);
+            this.Name = "Form1";
+            this.Text = "Distance Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.TextBox mphTextBox;
+        private System.Windows.Forms.TextBox hoursTextBox;
+        private System.Windows.Forms.Label mphLabel;
+        private System.Windows.Forms.Label hoursLabel;
+        private System.Windows.Forms.ListBox distanceListBox;
+    }
+}
+
---
+++ Distance Calculator (Number 1)/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Distance_Calculator__Number_1_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculateButton_Click(object sender, EventArgs e)
+        {
+            // Variables.
+            int speed;          // Will hold the speed in MPH.
+            int hours;          // Will hold the hours the car travels.
+            int distance;       // Will hold the distance that will be calculated.
+            int counter = 1;    // Is the counter that will be used to multiply by to find the distance traveled.
+
+            // If statements make sure that the values entered into the textboxes will be integers, if they're not, 
+            // a error message will be displayed with the else statements.
+            if(int.TryParse(mphTextBox.Text, out speed))
+            {
+                if(int.TryParse(hoursTextBox.Text, out hours))
+                { 
+                    // The counter will increment for each hour to correctly compute the distance traveled in each hour.
+                    while (counter < hours)
+                    { 
+                        // Calculates the distance.
+                       distance = speed * counter;
+                        // Displays the distance traveled in the ListBox.
+                        distanceListBox.Items.Add("After hour " + counter + " the distance is " + distance);
+                        counter++;
+                    }
+                } 
+                else
+                {
+                    MessageBox.Show("Invalid value for speed.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Invalid value for hours.");
+            }
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+    }
+}
